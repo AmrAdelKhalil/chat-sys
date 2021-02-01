e5600cddee8b7768c2867f1559aa99d7f2c3be6b
Amr Adel <amradelkhalil@gmail.com>
2021-02-01 18:19:25 +0200

    Use redis to lock the counter on create chat

--- chat-system/app/services/chat_services/create.rb
+++ chat-system/app/services/chat_services/create.rb
@@ -1,10 +1,15 @@
 module ChatServices
   class Create < ServicesBase
     def self.call(app)
-      Application.find(app.id).with_lock do
-        max_number = Chat.where(application_id: app.id).maximum(:chat_number) || 0
-        ChatCreateWorker.perform_async({'app_id' => app.id, 'chat_number' => max_number + 1})
-        Result.new(object: {chat_number: max_number + 1}, success: true)
+      Sidekiq.redis do |connection|
+        lock = RedisLock.new(app.token, redis: connection)
+        mx_chat_number = lock.value
+
+        mx_chat_number = mx_chat_number.nil? ? Chat.where(application_id: app.id).count : mx_chat_number.to_i
+
+        ChatCreateWorker.perform_async({'app_id' => app.id, 'chat_number' => mx_chat_number + 1})
+        lock.set(60, {value: (mx_chat_number + 1).to_s})
+        Result.new(object: {chat_number: mx_chat_number + 1}, success: true)
       end
     end
   end
